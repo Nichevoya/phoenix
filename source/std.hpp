f20199358545564d2dfb577ccaf643278efe6a15
Nichevoya <isaak.nicolas@epitech.eu>
2023-10-27 17:09:06 +0200

    add all standard headers

--- source/std.hpp
+++ source/std.hpp
@@ -2,32 +2,59 @@
 
 /* ----------------------------------- std ---------------------------------- */
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cstring>
-#include <string>
-
 #include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+#include <wctype.h>
 
-#include <filesystem>
-
-#include <dlfcn.h>
-
-#include <bits/stdc++.h>
+/* ---------------------- c++ standard library headers ---------------------- */
+#include <iostream>
+#include <stdexcept>
 #include <typeinfo>
+#include <utility>
+#include <algorithm>
 #include <bitset>
-#include <set>
-
-#include <memory>
-#include <thread>
-#include <mutex>
-
-#include <chrono>
-#include <random>
-
-#include <unordered_map>
-#include <vector>
-#include <array>
-#include <queue>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
 #include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <vector>
+

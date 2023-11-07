b8b536874c6318847458e3c3795be1d81702eb10
Isaak Nicolas <isaak.nicolas@epitech.eu>
2023-11-07 18:16:11 +0100

    platform specific library

--- source/library.hpp
+++ source/library.hpp
@@ -2,102 +2,108 @@
 
 #include "std.hpp"
 
-namespace phoenix {
-    
-    namespace load {
+#ifdef UNIX
 
-        template <class T>
-        class smart_type_allocator {
-            public:
-                typedef std::unique_ptr<T> (*unique)();
-                typedef std::shared_ptr<T> (*shared)();
-        };
-        
-        class handler {
-            public:
-                handler() = default;
-                handler(const handler &) = delete;
-                handler(const handler &&) = delete;
-                ~handler() = default;
+    namespace phoenix {
 
-                void *get(void) const { return _handler; }
+        namespace load {
 
-                void *const &open(const std::string &file) & { return (_handler = dlopen(file.c_str(), RTLD_LAZY)); }
+            template <class T>
+            class smart_type_allocator {
+                public:
+                    typedef std::unique_ptr<T> (*unique)();
+                    typedef std::shared_ptr<T> (*shared)();
+            };
 
-                template <typename T>
-                T load(std::string const &entry_point) const { return (T)dlsym(_handler, entry_point.c_str()); }
+            class handler {
+                public:
+                    handler() = default;
+                    handler(const handler &) = delete;
+                    handler(const handler &&) = delete;
+                    ~handler() = default;
 
-                int close() const { return dlclose(_handler); }
+                    void *get(void) const { return _handler; }
 
-                const char *error() const & { return dlerror(); }
+                    void *const &open(const std::string &file) & { return (_handler = dlopen(file.c_str(), RTLD_LAZY)); }
 
-            protected:
-            private:
-                void *_handler = nullptr;
-        };
+                    template <typename T>
+                    T load(std::string const &entry_point) const { return (T)dlsym(_handler, entry_point.c_str()); }
 
-        class library {
-            public:
-                library() = default;
-                library(const library &) = delete;
-                library(const library &&) = delete;
-                library(const std::string &file, const std::string &entry_point = "entry_point") : _file(file), _path(file.substr(file.find_last_of('/') + 1, file.size())), _entry_point(entry_point) {}
-                ~library()
-                {
-                    if (_handler.get() != nullptr) _handler.close();
-                }
+                    int close() const { return dlclose(_handler); }
 
-                template <typename T>
-                std::unique_ptr<T> load_unique(void)
-                {
-                    using unique = typename smart_type_allocator<T>::unique;
-                    if (!_handler.open(_file)) throw std::runtime_error(_handler.error());
-                    std::cout << "Handled library: " << name() << std::endl;
-                    return _handler.load<unique>(_entry_point)();
-                }
+                    const char *error() const & { return dlerror(); }
 
-                template <typename T>
-                std::unique_ptr<T> load_unique(const std::string &file, const std::string &entry_point = "entry_point")
-                {
-                    using unique = typename smart_type_allocator<T>::unique;
-                    if (!_handler.open(file)) throw std::runtime_error(_handler.error());
-                    _file = file;
-                    _path = _file.substr(_file.find_last_of('/') + 1, _file.size());
-                    std::cout << "Handled library: " << name() << std::endl;
-                    return _handler.load<unique>(_entry_point)();
-                }
+                protected:
+                private:
+                    void *_handler = nullptr;
+            };
 
-                template <typename T>
-                std::shared_ptr<T> load_shared(void)
-                {
-                    using shared = typename smart_type_allocator<T>::shared;
-                    if (!_handler.open(_file)) throw std::runtime_error(_handler.error());
-                    std::cout << "Handled library: " << name() << std::endl;
-                    return _handler.load<shared>(_entry_point)();
-                }
+            class library {
+                public:
+                    library() = default;
+                    library(const library &) = delete;
+                    library(const library &&) = delete;
+                    library(const std::string &file, const std::string &entry_point = "entry_point") : _file(file), _path(file.substr(file.find_last_of('/') + 1, file.size())), _entry_point(entry_point) {}
+                    ~library()
+                    {
+                        if (_handler.get() != nullptr) _handler.close();
+                    }
 
-                template <typename T>
-                std::shared_ptr<T> load_shared(const std::string &file, const std::string &entry_point = "entry_point")
-                {
-                    using shared = typename smart_type_allocator<T>::shared;
-                    if (!_handler.open(file)) throw std::runtime_error(_handler.error());
-                    _file = file;
-                    _path = _file.substr(_file.find_last_of('/') + 1, _file.size());
-                    std::cout << "Handled library: " << name() << std::endl;
-                    return _handler.load<shared>(_entry_point)();
-                }
+                    template <typename T>
+                    std::unique_ptr<T> load_unique(void)
+                    {
+                        using unique = typename smart_type_allocator<T>::unique;
+                        if (!_handler.open(_file)) throw std::runtime_error(_handler.error());
+                        std::cout << "Handled library: " << name() << std::endl;
+                        return _handler.load<unique>(_entry_point)();
+                    }
 
-                void free(void) const { _handler.close(); }
-                
-                const std::string &path(void) const & { return _path; }
+                    template <typename T>
+                    std::unique_ptr<T> load_unique(const std::string &file, const std::string &entry_point = "entry_point")
+                    {
+                        using unique = typename smart_type_allocator<T>::unique;
+                        if (!_handler.open(file)) throw std::runtime_error(_handler.error());
+                        _file = file;
+                        _path = _file.substr(_file.find_last_of('/') + 1, _file.size());
+                        std::cout << "Handled library: " << name() << std::endl;
+                        return _handler.load<unique>(_entry_point)();
+                    }
 
-                const std::string &name(void) const & { return _file; }
+                    template <typename T>
+                    std::shared_ptr<T> load_shared(void)
+                    {
+                        using shared = typename smart_type_allocator<T>::shared;
+                        if (!_handler.open(_file)) throw std::runtime_error(_handler.error());
+                        std::cout << "Handled library: " << name() << std::endl;
+                        return _handler.load<shared>(_entry_point)();
+                    }
 
-            private:
-                handler _handler;
-                std::string _file, _path, _entry_point;
-        };
+                    template <typename T>
+                    std::shared_ptr<T> load_shared(const std::string &file, const std::string &entry_point = "entry_point")
+                    {
+                        using shared = typename smart_type_allocator<T>::shared;
+                        if (!_handler.open(file)) throw std::runtime_error(_handler.error());
+                        _file = file;
+                        _path = _file.substr(_file.find_last_of('/') + 1, _file.size());
+                        std::cout << "Handled library: " << name() << std::endl;
+                        return _handler.load<shared>(_entry_point)();
+                    }
 
-    } // Load
+                    void free(void) const { _handler.close(); }
 
-} // phoenix
+                    const std::string &path(void) const & { return _path; }
+
+                    const std::string &name(void) const & { return _file; }
+
+                private:
+                    handler _handler;
+                    std::string _file, _path, _entry_point;
+            };
+
+        } // Load
+
+    } // phoenix
+
+#elif WIN32
+
+#endif

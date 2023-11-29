a580c18b46487a01071420b5d1b55ecc89c5555a
Isaak Nicolas <isaak.nicolas@epitech.eu>
2023-11-29 11:07:51 +0100

    to camel_case

--- source/input.hpp
+++ source/input.hpp
@@ -6,26 +6,26 @@ namespace phoenix {
     
     class input {
         public:                
-            friend std::ostream &operator<<(std::ostream &os, const input &input) { return os << input.inputMessage(); }
+            friend std::ostream &operator<<(std::ostream &os, const input &input) { return os << input.input_message(); }
             operator std::string() const  { return get(); }
 
             input(const input &) = delete;
             input(const input &&) = delete;
             input(const std::string &message = "") : _message(message)
             {
-                if (!_message.empty()) outputMessage();
-                getLine();
-                inputMessage();
+                if (!_message.empty()) output_message();
+                getline();
+                input_message();
             }
             ~input() = default;
 
             std::string &get(void) { return _input; }
             const std::string &get(void) const { return _input; }
 
-            void outputMessage(void) const { std::cout << _message << std::endl; }
-            const std::string &inputMessage(void) const { return get(); }
+            void output_message(void) const { std::cout << _message << std::endl; }
+            const std::string &input_message(void) const { return get(); }
 
-            void getLine(void) { std::getline(std::cin, get()); }
+            void getline(void) { std::getline(std::cin, get()); }
         
         protected:
         private:

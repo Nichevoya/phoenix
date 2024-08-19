14a0ec2a65637a72119000e656a4616049cfa11c
Isaak Nicolas <isaak.nicolas@epitech.eu>
2024-08-19 19:47:54 +0200

    [~] add multiple input from same input object, prompt input, remove unecessary code

--- source/input.hpp
+++ source/input.hpp
@@ -6,31 +6,50 @@ namespace phoenix {
     
     class input {
         public:                
-            friend std::ostream &operator<<(std::ostream &os, const input &input) { return os << input.input_message(); }
-            operator std::string() const  { return get(); }
+            friend std::ostream &operator<<(std::ostream &os, const input &input) { return os << input.get(); }
+            operator std::string() const  { return _input; }
 
             input(const input &) = delete;
             input(const input &&) = delete;
-            input(const std::string &message = "") : _message(message)
+            input(const std::string &message = "", const std::string &prompt = "") : _message(message), _prompt(prompt) 
             {
-                if (!_message.empty()) output_message();
+                if (!_message.empty()) 
+                    output_message();
+                
+                if (!prompt.empty())
+                    this->prompt();
+                
                 getline();
-                input_message();
             }
+
             ~input() = default;
 
-            std::string &get(void) { return _input; }
             const std::string &get(void) const { return _input; }
+            const std::string &get(const std::string &message = "", const std::string &prompt = "") 
+            {
+                _input = "";
+                _message = message;
+                _prompt = prompt;
 
-            void output_message(void) const { std::cout << _message << std::endl; }
-            const std::string &input_message(void) const { return get(); }
+                if (!_message.empty()) 
+                    output_message();
+                
+                if (!prompt.empty())
+                    this->prompt();
+                
+                getline();
 
-            void getline(void) { std::getline(std::cin, get()); }
+                return _input;
+            }
+
+            void prompt(void) const { std::cout << _prompt; }
+            void output_message(void) const { std::cout << _prompt << _message << std::endl; }
+
+            void getline(void) { std::getline(std::cin, _input); }
         
         protected:
         private:
-            std::string _input;
-            const std::string &_message = "";
+            std::string _input = "", _message = "", _prompt = "";
     };
 
 } // phoenix

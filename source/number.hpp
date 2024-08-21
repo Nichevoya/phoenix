45e8a967c1143c5246fdca7b128da28d0537a213
Isaak Nicolas <isaak.nicolas@epitech.eu>
2024-08-21 10:06:09 +0200

    [~] getters and setters overload themselves

--- source/number.hpp
+++ source/number.hpp
@@ -1,49 +1,44 @@
 #pragma once
 
-#include "std.hpp"
-
 namespace phoenix {
 
     template <typename T = int32_t>
     class number {
         public:
-            friend std::ostream& operator<<(std::ostream& os, const number<T>& number) { return os << std::fixed << std::setprecision(number.precision()) << number.get(); }
+            friend std::ostream& operator<<(std::ostream& os, const number<T>& number) { return os << std::fixed << std::setprecision(number.precision()) << number.value(); }
 
-            number<T> operator+(const number<T> &other) const { return number<T>(get() + other.get()); }
-            number<T> operator-(const number<T> &other) const { return number<T>(get() - other.get()); }
-            number<T> operator*(const number<T> &other) const { return number<T>(get() * other.get()); }
-            number<T> operator/(const number<T> &other) const { return other.get() != 0 ? number<T>(get() / other.get()) : number<T>(0); }
+            number<T> operator+(const number<T> &other) const { return number<T>(value() + other.value()); }
+            number<T> operator-(const number<T> &other) const { return number<T>(value() - other.value()); }
+            number<T> operator*(const number<T> &other) const { return number<T>(value() * other.value()); }
+            number<T> operator/(const number<T> &other) const { return other.value() != 0 ? number<T>(value() / other.value()) : number<T>(0); }
             
-            number<T> operator+=(const number<T> &other) const { return number<T>(get() += other.get()); }
-            number<T> operator-=(const number<T> &other) const { return number<T>(get() -= other.get()); }
-            number<T> operator*=(const number<T> &other) const { return number<T>(get() *= other.get()); }
-            number<T> operator/=(const number<T> &other) const { return other.get() != 0 ? number<T>(get() /= other.get()) : number<T>(0); }
+            number<T> operator+=(const number<T> &other) const { return number<T>(value() += other.value()); }
+            number<T> operator-=(const number<T> &other) const { return number<T>(value() -= other.value()); }
+            number<T> operator*=(const number<T> &other) const { return number<T>(value() *= other.value()); }
+            number<T> operator/=(const number<T> &other) const { return other.value() != 0 ? number<T>(value() /= other.value()) : number<T>(0); }
 
-            bool operator==(const number<T> &other) const { return get() == other.get(); }
-            bool operator!=(const number<T> &other) const { return get() != other.get(); }
+            bool operator==(const number<T> &other) const { return value() == other.value(); }
+            bool operator!=(const number<T> &other) const { return value() != other.value(); }
 
 
             number(): _value(0), _precision(2) {}
             number(const T &value, const int32_t &precision = 2) : _value(value), _precision(precision) {}
 
-            void set(const T &value) { _value = value; }
+            T &value(void) { return _value; }
+            const T &value(void) const { return _value; }
+            void value(const T &value) { _value = value; }            
 
             void precision(int32_t precision) { _precision = precision; }
-            
-            T get(void) { return _value; }
-
-            T get(void) const { return _value; }
-
-            const int32_t &precision(void) const & { return _precision; }
+            const short &precision(void) const & { return _precision; }
 
             const std::string string(void) const
             {
                 std::stringstream stream;
-                stream << std::fixed << std::setprecision(precision()) << get();
+                stream << std::fixed << std::setprecision(precision()) << value();
                 return stream.str();
             }
 
-            void type_action(void) { type_specific_operation(get()); }
+            void type_action(void) { type_specific_operation(value()); }
 
         private:            
             void type_specific_operation(int32_t)
@@ -68,7 +63,7 @@ namespace phoenix {
             }
 
             T _value;
-            int32_t _precision;
+            short _precision;
 
     };    
     
--- source/random.hpp
+++ source/random.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "std.hpp"
 #include "number.hpp"
 
 namespace phoenix {
@@ -18,10 +17,10 @@ namespace phoenix {
     template <>
     class random<int32_t> : public number<int32_t> {
         public:
-            friend std::ostream &operator<<(std::ostream &os, const random<int32_t> &value) { return os << value.get(); }
+            friend std::ostream &operator<<(std::ostream &os, const random<int32_t> &value) { return os << value.value(); }
             
-            random() : _rng(_seed()), _dist(-RAND_MAX, RAND_MAX) {  set(_dist(_rng)); }
-            random(const int32_t &from, const int32_t &to) : _rng(_seed()), _dist(from, to) { set(_dist(_rng)); }
+            random() : _rng(_seed()), _dist(-RAND_MAX, RAND_MAX) {  value(_dist(_rng)); }
+            random(const int32_t &from, const int32_t &to) : _rng(_seed()), _dist(from, to) { value(_dist(_rng)); }
             ~random() = default;
     
         protected:
@@ -34,10 +33,10 @@ namespace phoenix {
     template <>
     class random<float> : public number<float> {
         public:
-            friend std::ostream &operator<<(std::ostream &os, const random<float> &value) { return os << value.get(); }
+            friend std::ostream &operator<<(std::ostream &os, const random<float> &value) { return os << value.value(); }
             
-            random() : number<float>(0.0f), _rng(_seed()), _dist(-10.0f, 10.0f) {  set(_dist(_rng)); }
-            random(const float &from, const float &to) : number<float>(0.0f), _rng(_seed()), _dist(from, to) { set(_dist(_rng)); }
+            random() : number<float>(0.0f), _rng(_seed()), _dist(-10.0f, 10.0f) {  value(_dist(_rng)); }
+            random(const float &from, const float &to) : number<float>(0.0f), _rng(_seed()), _dist(from, to) { value(_dist(_rng)); }
             ~random() = default;
     
         protected:
@@ -50,10 +49,10 @@ namespace phoenix {
     template <>
     class random<double> : public number<double> {
         public:
-            friend std::ostream &operator<<(std::ostream &os, const random<double> &value) { return os << value.get(); }
+            friend std::ostream &operator<<(std::ostream &os, const random<double> &value) { return os << value.value(); }
             
-            random() : number<double>(0.0f), _rng(_seed()), _dist(-10.0f, 10.0f) {  set(_dist(_rng)); }
-            random(const double &from, const double &to) : number<double>(0.0f), _rng(_seed()), _dist(from, to) { set(_dist(_rng)); }
+            random() : number<double>(0.0f), _rng(_seed()), _dist(-10.0f, 10.0f) {  value(_dist(_rng)); }
+            random(const double &from, const double &to) : number<double>(0.0f), _rng(_seed()), _dist(from, to) { value(_dist(_rng)); }
             ~random() = default;
     
         protected:

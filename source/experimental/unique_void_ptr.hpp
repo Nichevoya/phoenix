90c6b6333d550e7a941287ebfb04c89e065aed49
Isaak Nicolas <isaak.nicolas@epitech.eu>
2024-11-11 11:16:52 +0100

    [+] unique_void_ptr getter

--- source/experimental/unique_void_ptr.hpp
+++ source/experimental/unique_void_ptr.hpp
@@ -9,6 +9,12 @@ namespace phoenix {
         using unique_void_ptr = std::unique_ptr<void, void (*)(const void *)>;
 
         template<typename T>
+        T &get_unique_void(const unique_void_ptr &ptr) 
+        {
+            return *static_cast<T *>(ptr.get());
+        }
+
+        template<typename T>
         unique_void_ptr unique_void(T *ptr) 
         {
             return unique_void_ptr(ptr, [] (const void *data) {

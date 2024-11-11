f8b1f03bdb6d645ea5512a4b36467bd9b42c6040
Isaak Nicolas <isaak.nicolas@epitech.eu>
2024-11-11 12:25:08 +0100

    [~] adjust precision value type, inline string method

--- source/number.hpp
+++ source/number.hpp
@@ -22,23 +22,17 @@ namespace phoenix {
             bool operator==(const number<T> &other) const { return value() == other.value(); }
             bool operator!=(const number<T> &other) const { return value() != other.value(); }
 
-
             number(): _value(0), _precision(2) {}
-            number(const T &value, const int32_t &precision = 2) : _value(value), _precision(precision) {}
+            number(const T &value, const int16_t &precision = 2) : _value(value), _precision(precision) {}
 
             T &value(void) { return _value; }
             const T &value(void) const { return _value; }
             void value(const T &value) { _value = value; }            
 
-            void precision(int32_t precision) { _precision = precision; }
-            const short &precision(void) const & { return _precision; }
+            void precision(int16_t precision) { _precision = precision; }
+            const int16_t &precision(void) const & { return _precision; }
 
-            const std::string string(void) const
-            {
-                std::stringstream stream;
-                stream << std::fixed << std::setprecision(precision()) << value();
-                return stream.str();
-            }
+            inline std::string string(void) const { return (std::stringstream() << std::fixed << std::setprecision(precision()) << value()).str(); }
 
             void type_action(void) { type_specific_operation(value()); }
 
@@ -65,7 +59,7 @@ namespace phoenix {
             }
 
             T _value;
-            short _precision;
+            int16_t _precision;
 
     };    
     
